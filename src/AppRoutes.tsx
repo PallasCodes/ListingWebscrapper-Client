beea20e94bd4ff93d8994f929f77e3ef72fa35ff
pallascodes <pallascodes@gmail.com>
2025-02-25 11:15:20 -0600

    feat: react router dom installed

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+}
